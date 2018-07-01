f161f171d6499e95848cb2419bdd4458d010b55a
Your Name <you@example.com>
2018-07-01 15:14:23 +0200

    commentaires

--- wysiwig.js
+++ wysiwig.js
@@ -1,6 +1,6 @@
     (function($) {
 
-        function line_through(clicked_deco) {
+        function line_through() {
             if (typeof line_through.counter === "undefined") {
                 line_through.counter = 0;
             }
@@ -10,11 +10,9 @@
                 $("textarea").css("text-decoration", "none");
             }
             line_through.counter += 1;
-            clicked_deco += 2;
-            return clicked_deco;
         }
 
-        function underline(clicked_deco) {
+        function underline() {
             if (typeof underline.counter === "undefined") {
                 underline.counter = 0;
             }
@@ -24,8 +22,6 @@
                 $("textarea").css("text-decoration", "none");
             }
             underline.counter += 1;
-            clicked_deco += 1;
-            return clicked_deco;
         }
 
         function bold() {
@@ -53,49 +49,48 @@
         }
 
         $.fn.my_wysiwyg = function (options) {
-            var button_ids = new Array;
-            var i = 0;
-            if ($.isPlainObject(options)) {
-                if ($.isArray(options.buttons)) {
-                    var buttons_div = document.createElement("div");
-                    options.buttons.forEach(function(value) {
+            var button_ids = new Array; //On crée un tableau qui servira a stocker nos boutons
+            var i = 0; //On initialise le compteur
+            if ($.isPlainObject(options)) { //On vérifie que le paramètre options passé en argument est bien un objet
+                if ($.isArray(options.buttons)) { //On vérifie que l'on a un array nommé buttons a l'intérieur de notre objet options
+                    var buttons_div = document.createElement("div"); //On crée la div pour stocker les boutons
+                    options.buttons.forEach(function(value) { //Pour chacun des éléments (value) du tableau buttons dans l'objet options :
                         if (value !== "color") {
-                            var button = document.createElement("button");
+                            var button = document.createElement("button"); //On crée un bouton si l'élément est différent de "color"
                         }
                         if (value === "color") {
-                            var button = document.createElement("input");
+                            var button = document.createElement("input"); //On crée un input si l'élement est égal à "color"
+                            button.setAttribute("type", "color"); //On donne a l'input le type "color" pour pouvoir choisir la couleur
                         }
-                        button.id = value;
+                        button.id = value; //On donne un élément un id correspondant à son nom (l'élément "color" aura l'id "color" et ainsi de suite)
                         if (value !== "color") {
-                            button.innerHTML = value;
+                            button.innerHTML = value; //Si ce n'est pas color le texte du bouton correspondra a son nom
                         }
-                        if (value === "color") {
-                            button.setAttribute("type", "color");
-                        }
-                        buttons_div.appendChild(button);
+                        buttons_div.appendChild(button); //On ajoute le bouton dans la div
+                        //On recommence jusqu'à ce qu'on ait fait tous les éléments du tableau buttons
                     });
-                    document.body.insertBefore(buttons_div, $.this);
-                    options.buttons.forEach(function(value) {
+                    document.body.insertBefore(buttons_div, $.this); //On insère la div avant $.this, qui correspond à $("textarea") dans ce contexte
+                    options.buttons.forEach(function(value) { //Une fois que les boutons sont insérés, on les stocke dans le tableau créé plus tôt
                         button_ids[i] = document.getElementById(value);
                         i += 1;
+                        //Ça permet d'avoir un tableau qui contient les boutons en mode document.children[0] toussa
                     });
-                    console.log(button_ids);
-                    button_ids.forEach(function(button) {
-                        var clicked_deco = 0;
-                        button.addEventListener("click", function () {
+                    button_ids.forEach(function(button) { //Pour chacun des boutons qu'on vient de créer :
+                        button.addEventListener("click", function () { //On ajoute un évènement onclick
                             if (button.id === "italic") {
-                                italic();
+                                italic(); // On applique la fonction italic() sur le bouton "italic"
                             } else if (button.id === "bold") {
-                                bold();
+                                bold();//la fonction bold() sur le bouton "bold"
                             } else if (button.id === "line_through") {
-                                line_through(clicked_deco);
+                                line_through(); //etc
                             } else if (button.id === "underline") {
-                                underline(clicked_deco);
+                                underline();
                             }
+                            //si le bouton ne correspond a aucune de nos conditions, il n'aura pas d'évènement onclick
                         });
-                        button.addEventListener("change", function () {
+                        button.addEventListener("change", function () { // On utilise un évènement onchange pour la couleur
                             if (button.id === "color") {
-                                $("textarea").css("color", button.value);
+                                $("textarea").css("color", button.value); //Lorsque la valeur est changée, la couleur du texte prend la valeur de la couleur choisie
                             }
                         });
                     });
@@ -105,9 +100,9 @@
     })(jQuery);
 
 $("textarea").my_wysiwyg(
-    {
+  /*l'objet options :*/  { 
         option1: "valeur1",
         option2: "valeur2",
-        buttons: ["bold", "italic", "color", "line_through", "underline"]
+        buttons: ["bold", "italic", "color", "line_through", "underline"] //Le tableau buttons de l'objet options
     }
 );
